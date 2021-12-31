1b7683107e91e74bf7f355c2cb5378ec628d4817
R-icntay <eric.wanjau@studentpartner.com>
2021-12-31 00:39:04 +0000

    Update Question 5.R

--- ml-basics/02 - Regression/ottr_solutions_notebooks/student/tests/Question 5.R
+++ ml-basics/02 - Regression/ottr_solutions_notebooks/student/tests/Question 5.R
@@ -10,7 +10,18 @@ test = list(
       code = {
         ## Test ##
         test_that('recipe specification is correct', {
-          expect_output(print(estate_recipe), "Recipe\n\nInputs:\n\n\nOperations:\n\nDelete terms transaction_date\nVariable mutation\nCentering and scaling for all_numeric_predictors()", fixed = TRUE)
+          
+          # Test for step_rm
+          expect_equal(attr(estate_recipe[["steps"]][[1]], "class"), c("step_rm","step"))
+          expect_equal(as_label(estate_recipe[["steps"]][[1]][["terms"]][[1]]), "transaction_date")
+          
+          # Test for step_mutate
+          expect_equal(attr(estate_recipe[["steps"]][[2]], "class"), c("step_mutate","step"))
+          expect_equal(as_label(estate_recipe[["steps"]][[2]][["inputs"]][["local_convenience_stores"]]), "factor(local_convenience_stores)")
+          
+          # Test for step_normalize
+          expect_equal(attr(estate_recipe[["steps"]][[3]], "class"), c("step_normalize","step"))
+          expect_equal(as_label(estate_recipe[["steps"]][[3]][["terms"]][[1]]), "all_numeric_predictors()")
           
           
         })
